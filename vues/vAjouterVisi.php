939a3153b1e05b7e952b9edc603a2172afa5dfce
NoaSillam <noasillam@gmail.com>
2022-03-27 19:21:00 +0200

    changement orthographe prenom

--- vues/vAjouterVisi.php
+++ vues/vAjouterVisi.php
@@ -18,7 +18,7 @@
     <legend>Entrez les donnees sur le visiteur à ajouter </legend>
     <label> ID : </label> <input type="text" placeholder="Entrer l'id à"name="VIS_MATRICULE" size="10" /><br />
     <label>Nom :</label> <input type="text" name="VIS_NOM" size="20" /><br />
-    <label>Nom :</label> <input type="text" name="VIS_PRENOM" size="20" /><br />
+    <label>Prenom :</label> <input type="text" name="VIS_PRENOM" size="20" /><br />
     <label>Mot de Pass:</label> <input type="password" name="mdp" size="10" /><br />
     <label>Mot de Pass1:</label> <input type="password" name="mdp1" size="10" /><br />
     <label>Mail:</label> <input type="text" name="adresseMail" size="50" /><br />
